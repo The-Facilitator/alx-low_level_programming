65a12e709118f7fd4709c63a861e78e97cd1fbf5
Akor Hilary Okwute <cyberakor@gmail.com>
2023-02-25 19:13:09 -0800

    My first commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,9 +3,9 @@
 /**
  * main - main block
  * Description: This program prints all
- * single digit numbers of base 10
- * starting from 0
+ * single digit numbers of base 10 starting from 0
  * followed by a new line.
+ * Return: 0
  */
 
 int main(void)
