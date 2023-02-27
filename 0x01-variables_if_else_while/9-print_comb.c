ef23173859b94f1e4feadc4bbb9ba424566b1b0c
Akor Hilary Okwute <cyberakor@gmail.com>
2023-02-27 11:33:31 -0800

    My first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main - main block
+ * Description: This program prints:
+ * All possiblecombination of single digit numbers
+ * The numbers are separated by ',' followed by a space
+ * The numbers are printed in ascending order
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
 
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
 
+		if (n == 9)
+		{
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
