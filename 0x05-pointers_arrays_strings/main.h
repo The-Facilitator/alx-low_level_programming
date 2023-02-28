e7fc8f0e299d8ea7560461a86eaa2060ab242f6b
Akor Hilary Okwute <cyberakor@gmail.com>
2023-02-27 19:45:19 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(inta *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -13,4 +13,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dset, char *src);
 
-#endif
